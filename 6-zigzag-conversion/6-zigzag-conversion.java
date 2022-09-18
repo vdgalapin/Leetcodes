cf6c36f323914abb0ad57eb4ef59ca0c2cbad19e
Vyron Galapin <59787986+vdgalapin@users.noreply.github.com>
2022-09-18 15:42:41 -0500

    Time: 132 ms (8.13%), Space: 114.9 MB (5.04%) - LeetHub

---
+++ 6-zigzag-conversion/6-zigzag-conversion.java
@@ -0,0 +1,55 @@
+class Solution {
+    public String convert(String s, int numRows) {
+        
+        if (numRows == 1) {
+            return s;
+        }
+        int columns = s.length();
+        int rows = numRows;
+        
+        Character[][] zigzag = new Character[rows][columns];
+        
+        int counter = 0;
+        int columnCounter = 0;
+        while(counter < s.length()) {
+            
+            int rowCounter = 0;
+            
+            while((rows > rowCounter) && (counter < s.length())) {
+                
+                zigzag[rowCounter][columnCounter] = s.charAt(counter);
+                
+                counter += 1;
+                rowCounter += 1;                                                
+            }
+            
+            rowCounter -= 1;
+            
+            while((rowCounter > 0) && (counter < s.length())) {
+                
+                columnCounter += 1;
+                rowCounter -= 1;
+                zigzag[rowCounter][columnCounter] = s.charAt(counter);
+                
+                counter += 1;   
+            }
+            
+            if(counter < s.length()) {
+                counter -= 1;
+            }
+            
+            
+        }
+        String result = "";
+    for(int x = 0; x < zigzag.length; x++) {
+        for(int y = 0; y < zigzag[x].length; y++) {
+            if (zigzag[x][y] != null) {
+                result += zigzag[x][y];
+            }
+        }
+     
+    }
+    
+        return result;
+    }
+}
