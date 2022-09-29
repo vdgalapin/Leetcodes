edea669cb9c334f0dd736f363dfd66211df5f71b
Vyron Galapin <59787986+vdgalapin@users.noreply.github.com>
2022-09-28 20:35:46 -0500

    Time: 0 ms (100.00%), Space: 42.4 MB (52.78%) - LeetHub

--- 66-plus-one/66-plus-one.java
+++ 66-plus-one/66-plus-one.java
@@ -1,44 +1,61 @@
 class Solution {
     public int[] plusOne(int[] digits) {
         
-        if(digits.length == 1) {
-            if(digits[0] == 9) {
-                int[] instant_result = new int[2];
-                instant_result[0] = 1;
-                instant_result[1] = 0;
-                return instant_result;
-            } else {
-                digits[0] = digits[0] + 1;
+        int length = digits.length;
+        
+        for(int x = length - 1; x >= 0; x--) {
+            
+            if(digits[x] <  9) {
+                digits[x]++;
                 return digits;
             }
+            
+            digits[x] = 0;
+            
         }
         
-        int plus = 1;
-        for(int x = digits.length - 1; x > 0; x--) {
-            if(digits[x] == 9 && plus == 1) {               
-                digits[x] = (digits[x] + plus) % 10;
-                plus = 1;
-            } else {
-                digits[x] = digits[x] + plus;
-                plus = 0;
-            }
-        }
+        int[] result = new int[length+1];
+        result[0] = 1;
+        
+        return result;
+//         if(digits.length == 1) {
+//             if(digits[0] == 9) {
+//                 int[] instant_result = new int[2];
+//                 instant_result[0] = 1;
+//                 instant_result[1] = 0;
+//                 return instant_result;
+//             } else {
+//                 digits[0] = digits[0] + 1;
+//                 return digits;
+//             }
+//         }
+        
+//         int plus = 1;
+//         for(int x = digits.length - 1; x > 0; x--) {
+//             if(digits[x] == 9 && plus == 1) {               
+//                 digits[x] = (digits[x] + plus) % 10;
+//                 plus = 1;
+//             } else {
+//                 digits[x] = digits[x] + plus;
+//                 plus = 0;
+//             }
+//         }
       
-        if(plus == 1) {
-            if(digits[0] == 9) {
-                int[] result = new int[digits.length + 1];
-                result[0] = 1;
-                result[1] = 0;
-                for(int x = 2; x < result.length; x++) {
-                    result[x] = digits[x-1];
-                }
-                return result;
-            } else {
-                digits[0] = digits[0] + 1;
-                return digits;
-            }
-        } else {
-            return digits;
-        }   
+//         if(plus == 1) {
+//             if(digits[0] == 9) {
+//                 int[] result = new int[digits.length + 1];
+//                 result[0] = 1;
+//                 result[1] = 0;
+//                 for(int x = 2; x < result.length; x++) {
+//                     result[x] = digits[x-1];
+//                 }
+//                 return result;
+//             } else {
+//                 digits[0] = digits[0] + 1;
+//                 return digits;
+//             }
+//         } else {
+//             return digits;
+//         }   
     }
 }
